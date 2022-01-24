1b24fce518e29d4d81792a198e6932c567c204ff
unlimit1984 <unlimit_rmc@mail.ru>
2022-01-24 15:18:48 +0100

    [js-refresher] 15 Classes

---
+++ Lesson-15-Classes/main.js
@@ -0,0 +1,20 @@
+class Human {
+    constructor() {
+        this.gender = 'male';
+    }
+
+    printGender(){
+        console.log(this.gender);
+    }
+}
+
+class Person extends Human {
+    constructor() {
+        super();
+        this.name = 'Max';
+        this.gender = 'female';
+    }
+    printMyName(){
+        console.log(this.name);
+    }
+}
