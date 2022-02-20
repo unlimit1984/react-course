43652fb9274136a08e0a35d4da5b380344af509e
unlimit1984 <unlimit_rmc@mail.ru>
2022-02-20 18:25:36 +0100

    [js-refresher] 14 Exports and Imports
    - Addition with comma

--- Lesson-014-Exports-and-Imports/app.js
+++ Lesson-014-Exports-and-Imports/app.js
@@ -1,8 +1,12 @@
 import person from './person.js'
 import prs from './person.js'
 
-import { baseData } from './utility.js'
-import { clean } from './utility.js'
+
+// import { baseData } from './utility.js'
+// import { clean } from './utility.js'
+
+//OR
+import { baseData, clean } from './utility.js'
 
 //OR
 import { clean as myClean} from './utility.js'
